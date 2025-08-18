78c952da1b3f44126b468a17d2e83296a2306230
a.sysoeva <a.sysoeva@eco-fin.eu>
2025-08-18 15:15:08 +0300

    fix: first try

---
+++ src/test/java/basics/lesson1/FirstClass.java
@@ -0,0 +1,10 @@
+package basics.lesson1;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstClass {
+    @Test
+    public void printNewTest() {
+        System.out.println("Test");
+    }
+}
